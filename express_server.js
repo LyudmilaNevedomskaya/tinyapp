e5b699f184e40ed3d8bf317b5dfa74366bd19aeb
Lyudmila Nevedomskaya <milo4kamoya@gmail.com>
2021-08-03 00:51:02 +0000

    Added post request

--- express_server.js
+++ express_server.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
+
 
 app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended: true}));
+
+function generateRandomString() {
+
+}
+
+function generateRandomString(length, chars) {
+  var result = '';
+  for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+  return result;
+}
+var rString = generateRandomString(6, '0123456789abcdefghijklmnopqrstuvwxyz');
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -34,6 +50,15 @@ app.get("/set", (req, res) => {
    const templateVars = {urls: urlDatabase};
    res.render('urls_index', templateVars);
  });
+
+ app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
  app.get('/urls/:shortURL', (req, res) => {
   const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
